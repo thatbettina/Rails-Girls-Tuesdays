6e74c6bb8b6f5d799b277ec5c43e1059ed977f74
Bettina <bettina.shzu@gmail.com>
2013-04-23 21:25:54 +0200

    first commit after adding git back

---
+++ README.md
@@ -0,0 +1,4 @@
+# Number1 app
+This is the number1 app the rails girls project group of @til and me are working one. It is an app involving places and who knows what else! But we are sure it is going to be amazing.
+
+As we don't use git in the group yet this is used as a point of reference and to share code for til and me as we can't always both be there.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Number1::Application.config.secret_token = '77329f3ed02211e19c004d757c191e2fd4222889703e5d16a2aae806545dd8051f269d6c4c70d9e57c178ab92a804de8f4d39ade5ebcd2540b4d5e6efb3abfd3'
